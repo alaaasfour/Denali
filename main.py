e8738a53f8ba8060b78dc821b68d360198291a16
Alaa Asfour <alaa.asfour94@outlook.com>
2024-01-01 18:02:34 -0500

    Adding the complete task feature to the app

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
 todos = []
 
 while True:
-    user_action = input("Type add, edit, show or exit ")
+    user_action = input("Type add, edit, show, complete or exit ")
     user_action = user_action.strip()
     match user_action:
         case 'add':
             todo = input("Enter a todo ")
             todos.append(todo)
+
         case 'show':
             for index, item in enumerate(todos):
                 item = item.title()
                 row = f"{index + 1}. {item}"
                 print(row)
+
         case 'edit':
             number = int(input("Number of the todo to edit: "))
             number = number - 1
             new_todo = input("Enter a new todo item: ")
             todos[number] = new_todo
+
+        case 'complete':
+            number = int(input("Number of the todo to complete: "))
+            todos.pop(number - 1)
+            print("Task completed.")
 
         case 'exit':
             break
